d80df77b0b77bf54d08241f5410ccece3d425c0e
iamshabell <91321698+iamshabell@users.noreply.github.com>
2024-08-21 11:34:57 +0300

    exiting QEMU after tests

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,39 @@
 use core::panic::PanicInfo;
 mod vga_buffer;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{info}");
@@ -23,19 +56,4 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-}
-
-#[test_case]
-fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
 }
